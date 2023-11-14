6c8ddbd991a39641ef17ead9256baa9dc29c4e48
sobczal2 <81999946+sobczal2@users.noreply.github.com>
2023-11-14 10:19:25 +0100

    tests

--- mobile/app/src/main/java/com/sobczal2/biteright/MainActivity.kt
+++ mobile/app/src/main/java/com/sobczal2/biteright/MainActivity.kt
@@ -3,13 +3,19 @@ package com.sobczal2.biteright
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sobczal2.biteright.ui.theme.BiteRightTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Kasia")
                 }
             }
         }
@@ -30,10 +36,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .background(Color.Red)
+            .padding(16.dp),
+        color = Color.Blue,
+        fontSize = 30.sp,
+        fontStyle = FontStyle.Italic
     )
 }
 
