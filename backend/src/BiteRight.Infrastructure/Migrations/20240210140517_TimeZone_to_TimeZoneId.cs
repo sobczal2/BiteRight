d433979f72c188b36679c3d89333d21c99869564
sobczal2 <81999946+sobczal2@users.noreply.github.com>
2024-02-10 23:58:29 +0100

    Rename column "time_zone" to "time_zone_id" and "usage" to "consumption"
    Renames two column names in the migration file using the RenameColumn() method. Specifically, it transforms "time_zone" to "time_zone_id" to present a more descriptive name, and changes "usage" to "consumption" to reflect a more accurate semantic meaning. The modification affects the UserProfile and Product domains.

---
+++ backend/src/BiteRight.Infrastructure/Migrations/20240210140517_TimeZone_to_TimeZoneId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiteRight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeZone_to_TimeZoneId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "time_zone",
+                schema: "user",
+                table: "profiles",
+                newName: "time_zone_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "time_zone_id",
+                schema: "user",
+                table: "profiles",
+                newName: "time_zone");
+        }
+    }
+}
---
+++ backend/src/BiteRight.Infrastructure/Migrations/20240210195059_Usage_to_consumption.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiteRight.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usage_to_consumption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "usage",
+                schema: "product",
+                table: "products",
+                newName: "consumption");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "consumption",
+                schema: "product",
+                table: "products",
+                newName: "usage");
+        }
+    }
+}
